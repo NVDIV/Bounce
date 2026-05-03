228ea6dd6af56094cf2894fd222dc6f349ff891c
Nadiia Rybak <nvdiv5@gmail.com>
2026-05-03 16:26:24 +0300

    console threads

--- src/main/java/org/example/Bounce.java
+++ src/main/java/org/example/Bounce.java
@@ -4,10 +4,29 @@ import javax.swing.*;
 
 public class Bounce {
     public static void main(String[] args) {
-        BounceFrame frame = new BounceFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        System.out.println("Thread name = " +
-                Thread.currentThread().getName());
+
+        for (int i = 0; i < 100; i++) {
+
+            Thread t1 = new DashThread();
+            Thread t2 = new PipeThread();
+
+            t1.start();
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println();
+        }
+
+//        BounceFrame frame = new BounceFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
+//        System.out.println("Thread name = " +
+//                Thread.currentThread().getName());
     }
 }
